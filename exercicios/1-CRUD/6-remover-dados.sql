3bccdc91a0a25b4cfdce60e9da33aca15e29a1de
TatyGotica <tatiany.garcia@unesp.br>
2025-03-30 23:14:38 +0000

    remover linhas de uma planilha sempre com uma condicao WHERE. DELETE FROM

--- exercicios/1-CRUD/6-remover-dados.sql
+++ exercicios/1-CRUD/6-remover-dados.sql
@@ -1,4 +1,3 @@
--- Da tabela invoice, remova o invoice cujo o InvoiceID corresponda ao valor 33
-
-
--- Da tabela invoice, remova todos os invoices cujo o Total seja menor do que 1
+-- remover linhas de uma planilha
+DELETE FROM tabela
+WHERE condicao;
