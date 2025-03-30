c487d78338fb5f716d01a91c55541340fb2f9930
TatyGotica <tatiany.garcia@unesp.br>
2025-03-30 22:45:39 +0000

    insert into e values

--- exercicios/1-CRUD/3-inserir-dados.sql
+++ exercicios/1-CRUD/3-inserir-dados.sql
@@ -1,4 +1,5 @@
--- Insira 3 classes salariais na tabela salario_classe
-
-
--- Insira 1 nova classe salarial, sem declarar o atributo id_salario
+INSERT INTO nome_tabela (coluna1, coluna2, coluna3...)
+VALUES (valor_coluna1, valor_coluna2, valor_coluna3...)
+(valor_coluna1, valor_coluna2, valor_coluna3...)
+(valor_coluna1, valor_coluna2, valor_coluna3...)
+(valor_coluna1, valor_coluna2, valor_coluna3...)
