b8357cadfa8fd6eab668eeaa2a953280075ce3a8
TatyGotica <tatiany.garcia@unesp.br>
2025-03-31 16:41:19 +0000

    Muitas casas decimais, usar função ROUND com a função de agregar valores numéricos.

--- exercicios/2-construindo-consultas/5-funcoes-agregadoras.sql
+++ exercicios/2-construindo-consultas/5-funcoes-agregadoras.sql
@@ -1,13 +1,21 @@
 --Funções para agregar valores númericos 
---AVG: Retorna a média aritmética dos valores das compras realizadas. Ela recebe como argumento a coluna, a qual eu gostaria de saber a média aritmética, exemplo: (Total) e vou nomear como as ticket_medio, porque quero saber o ticket medio.
---identifica o ticket médio das compras, por exemplo.
+--Se a função de agregar valor numérico
+--retornar muitas casas decimais,
+--fazer  para melhorar a visualização
+--usar função ROUND.
+--usar a função ROUND junto com AVG,
+--porque essa função recebe 
+--dois argumentos: 
+--1-o valor a ser arredondado
+--2- e o total de casas decimais que eu gostaria, que fosse apresentado.
+--Exemplo prático: ROUND(AVG(Total) , 2) as ticket_medio.
 SELECT
 BillingCountry as pais,
 SUM(Total) as soma_compras,
 COUNT(Total) as qnt_compras_realizadas,
 MIN(Total) as menor_compra,
 MAX(Total) as maior_compra,
-AVG(Total) as ticket_medio
+ROUND(AVG(Total),2) as ticket_medio
 FROM
 invoices
 WHERE
