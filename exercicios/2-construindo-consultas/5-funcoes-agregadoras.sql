fbe8e7025d82d62a6112d3689b5abcb304dc895a
TatyGotica <tatiany.garcia@unesp.br>
2025-03-31 16:04:16 +0000

    Função textual CONCAT: nova coluna resultado da concatenação (união) de outras colunas da tabela selecionada.

--- exercicios/2-construindo-consultas/5-funcoes-agregadoras.sql
+++ exercicios/2-construindo-consultas/5-funcoes-agregadoras.sql
@@ -1,10 +1,16 @@
 --Funções para manipulação de dados de tipo textual (string)
---LOWER: deixa os textos da coluna todo em letra minúscula. Converte as letras da coluna em minúsculas.
+--CONCAT. No SQLite é representado por:
+--|| ' ' ||
+-- usado para concatenar, unir colunas em uma só.
+-- coluna nova proveniente da concatenação da
+--coluna nome e sobrenome.
 
 SELECT DISTINCT
 CustomerID as id,
 LOWER (FirstName) as nome,
 UPPER (LastName) as sobrenome,
+FirstName || ' ' || LastName as 
+nome_completo,
 Address as endereco
 FROM customers
 WHERE
