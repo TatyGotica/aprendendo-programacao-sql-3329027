85fb16d8c96c63b4865c766949868a508f4d8eb1
TatyGotica <tatiany.garcia@unesp.br>
2025-03-31 15:43:48 +0000

    query condicional pratica

--- exercicios/2-construindo-consultas/3-condicional.sql
+++ exercicios/2-construindo-consultas/3-condicional.sql
@@ -1,4 +1,12 @@
+SELECT DISTINCT 
+Country as pais,
+State as estado_sigla,
 CASE
-WHEN condicao THEN faca isso
-ELSE faca outra coisa
-END AS titulo-coluna
+WHEN State = 'SP' THEN 'São Paulo'
+WHEN State = 'RJ' THEN 'Rio de Janeiro'
+ELSE 'Estado desconhecido'
+END AS estado
+FROM
+customers
+WHERE
+Country LIKE 'Brazil';
