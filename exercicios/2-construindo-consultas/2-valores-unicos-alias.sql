e4fd961eb5e26c920b3410d9d566fbf0690804e4
TatyGotica <tatiany.garcia@unesp.br>
2025-03-31 15:11:23 +0000

    Alias: nomes temporários para colunas com AS

--- exercicios/2-construindo-consultas/2-valores-unicos-alias.sql
+++ exercicios/2-construindo-consultas/2-valores-unicos-alias.sql
@@ -1,11 +1,11 @@
--- Neste exemplo vamos retornar várias colunas
+-- palavra-chave para criar nomes temporários
+-- para as colunas. 
+--Isso é bem útilquando temos títulos de colunas longos
+-- Clausula AS. A base está em inglês e quero dar um nome para a coluna em português, para cada coluna e é possível
+-- Isso é chamado de Alias, representado pela palavra AS,
+--seguido pelo nome da coluna.
 SELECT DISTINCT
-Country,
-Company
+Country AS pais,
+Company AS empresa
 FROM
 customers;
-
---Por exemplo: no Brasil temos 4 empresas,
--- então, apesar de termos o país Brasil repetido 4 vezes
---SÃO 4 COMBINAÇÕES ÚNICAS ENTRE EMPRESAS E PAÍS
-
