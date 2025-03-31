37281e658fee02dd25ef2b77b79f1391fbbee7e5
TatyGotica <tatiany.garcia@unesp.br>
2025-03-31 16:39:27 +0000

    AVG: função de média para ticket médio, por exemplo.

--- exercicios/2-construindo-consultas/5-funcoes-agregadoras.sql
+++ exercicios/2-construindo-consultas/5-funcoes-agregadoras.sql
@@ -1,12 +1,13 @@
 --Funções para agregar valores númericos 
---MIN: para saber qual foi a compra de menor valor.
---MAX: para saber com foi a compra de maior valor.
+--AVG: Retorna a média aritmética dos valores das compras realizadas. Ela recebe como argumento a coluna, a qual eu gostaria de saber a média aritmética, exemplo: (Total) e vou nomear como as ticket_medio, porque quero saber o ticket medio.
+--identifica o ticket médio das compras, por exemplo.
 SELECT
 BillingCountry as pais,
 SUM(Total) as soma_compras,
 COUNT(Total) as qnt_compras_realizadas,
 MIN(Total) as menor_compra,
-MAX(Total) as maior_compra
+MAX(Total) as maior_compra,
+AVG(Total) as ticket_medio
 FROM
 invoices
 WHERE
