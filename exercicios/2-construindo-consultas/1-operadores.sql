2c3443d764baace90bfdf5408a2f0db84a7af994
TatyGotica <tatiany.garcia@unesp.br>
2025-03-31 14:55:51 +0000

    operador OR

--- exercicios/2-construindo-consultas/1-operadores.sql
+++ exercicios/2-construindo-consultas/1-operadores.sql
@@ -1,5 +1,7 @@
---Ex: quero retornar das 2240 linhas da consulta, 
---apenas as referentes ao InvoiceID =3
+--Como funciona o AND, operador lógico.
+--cria filtros mais complexos.
+--Ele retorna todas as linhas que cumpram ambas as condições
+--estabelecidas.
 
 SELECT 
 TrackID,
@@ -9,4 +11,16 @@ UnitPrice + 2.11,
 (UnitPrice + 2.11) % 2
 FROM
 invoice_items
-WHERE InvoiceID = 3;
+WHERE InvoiceID = 1
+-- diferente é assim !=
+OR InvoiceID > 3;
+
+-- essa querie retorna todos os resultados
+-- cujo InvoiceID é maior do que três
+-- e que são diferentes de 10.
+-- Outro operador lógico é o OR, que retorna todas as linhas, onde as condições se cumpram.
+-- Para exemplificar o operador OR, troco o filtro WHERE para InvoiceID = 1
+-- e o OR sendo Invoice ID > 3.
+--Apaga o AND.
+--Como estamos usando o operador OR, ele vai retornar todas as linhas
+-- da tabela que os valores sejam iguais a 1 ou maiores do que 3.
