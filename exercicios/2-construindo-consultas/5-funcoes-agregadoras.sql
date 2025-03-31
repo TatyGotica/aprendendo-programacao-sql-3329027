5b3e2485c19f1a29ce5d97cc2c309c4398bcaeb9
TatyGotica <tatiany.garcia@unesp.br>
2025-03-31 16:30:14 +0000

    Função COUNT: conta quantidades.

--- exercicios/2-construindo-consultas/5-funcoes-agregadoras.sql
+++ exercicios/2-construindo-consultas/5-funcoes-agregadoras.sql
@@ -1,9 +1,12 @@
 --Funções para agregar valores númericos 
---SUM filtrado por condição.
--- ou seja: SUM + WHERE
+--COUNT: conta quantidades.
+-- Quantidade de compras que foram realizadas
+--COUNT conta, por exemplo, todas as linhas referentes a Áustria e que tiveram alguma compra realizada maior do que zero.
+
 SELECT
 BillingCountry as pais,
-SUM(Total) as soma_compras
+SUM(Total) as soma_compras,
+COUNT(Total) as qnt_compras_realizadas
 FROM
 invoices
 WHERE
