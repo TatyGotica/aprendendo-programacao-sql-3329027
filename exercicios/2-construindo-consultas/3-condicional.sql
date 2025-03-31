929dc2e1b55bd1a715a6a70b21a2648080d50170
TatyGotica <tatiany.garcia@unesp.br>
2025-03-31 15:44:31 +0000

    else é opcional na expressão condicional

--- exercicios/2-construindo-consultas/3-condicional.sql
+++ exercicios/2-construindo-consultas/3-condicional.sql
@@ -5,6 +5,7 @@ CASE
 WHEN State = 'SP' THEN 'São Paulo'
 WHEN State = 'RJ' THEN 'Rio de Janeiro'
 ELSE 'Estado desconhecido'
+--ELSE é opcional na expressão condicional
 END AS estado
 FROM
 customers
