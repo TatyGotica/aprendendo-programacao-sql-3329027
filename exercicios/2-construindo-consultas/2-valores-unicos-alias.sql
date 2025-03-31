8ffe29376bc3f0de1e9dc29f7a8f30530441028d
TatyGotica <tatiany.garcia@unesp.br>
2025-03-31 15:06:49 +0000

    combinaçao unica de colunas na clausula SELECT DISTINCT

--- exercicios/2-construindo-consultas/2-valores-unicos-alias.sql
+++ exercicios/2-construindo-consultas/2-valores-unicos-alias.sql
@@ -1,6 +1,11 @@
--- Neste exemplo vamos retornar todos os países preenchidos
--- na coluna country, mas sem repetição/duplicidade
-SELECT DISTINCT 
-Country
+-- Neste exemplo vamos retornar várias colunas
+SELECT DISTINCT
+Country,
+Company
 FROM
 customers;
+
+--Por exemplo: no Brasil temos 4 empresas,
+-- então, apesar de termos o país Brasil repetido 4 vezes
+--SÃO 4 COMBINAÇÕES ÚNICAS ENTRE EMPRESAS E PAÍS
+
