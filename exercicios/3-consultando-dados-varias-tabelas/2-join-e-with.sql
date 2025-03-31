011b478181a946330509423082343709aa371e04
TatyGotica <tatiany.garcia@unesp.br>
2025-03-31 17:16:30 +0000

    WITH: tabelas temporarias. Consultas complexas. vem antes do SELECT.

--- exercicios/3-consultando-dados-varias-tabelas/2-join-e-with.sql
+++ exercicios/3-consultando-dados-varias-tabelas/2-join-e-with.sql
@@ -1,5 +1,7 @@
 -- Crie uma consulta para realizar inner join com as tabelas "tracks", "albums" e "artists"
 -- Refatore a consulta anterior usando a cláusula "with" e verifique o total de músicas existente na base de terminado artista, ex: Caetano Veloso
+WITH musica as ( 
+
 SELECT
 tra.TrackId as id,
 tra.Name as musica,
@@ -8,4 +10,12 @@ art.Name as artista
 FROM
 tracks as tra
 INNER JOIN albums as alb ON tra.AlbumId = alb.AlbumId
-INNER JOIN artists AS art ON art.ArtistId = alb.ArtistId;
+INNER JOIN artists AS art ON art.ArtistId = alb.ArtistId)
+
+SELECT
+artista,
+COUNT(musica) as qnt_total_musica
+FROM
+musica
+WHERE
+artista LIKE 'Caetano%';
