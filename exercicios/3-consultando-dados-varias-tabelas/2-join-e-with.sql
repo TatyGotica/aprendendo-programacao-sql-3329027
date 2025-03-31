fe1a75b535ed291e556c297dfcccc4dafd02df1a
TatyGotica <tatiany.garcia@unesp.br>
2025-03-31 17:07:35 +0000

    INNER JOIN: junção entre 3 colunas

--- exercicios/3-consultando-dados-varias-tabelas/2-join-e-with.sql
+++ exercicios/3-consultando-dados-varias-tabelas/2-join-e-with.sql
@@ -1,4 +1,11 @@
 -- Crie uma consulta para realizar inner join com as tabelas "tracks", "albums" e "artists"
-
-
 -- Refatore a consulta anterior usando a cláusula "with" e verifique o total de músicas existente na base de terminado artista, ex: Caetano Veloso
+SELECT
+tra.TrackId as id,
+tra.Name as musica,
+alb.Title as album,
+art.Name as artista
+FROM
+tracks as tra
+INNER JOIN albums as alb ON tra.AlbumId = alb.AlbumId
+INNER JOIN artists AS art ON art.ArtistId = alb.ArtistId;
