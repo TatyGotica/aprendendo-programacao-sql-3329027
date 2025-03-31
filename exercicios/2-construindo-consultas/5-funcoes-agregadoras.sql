d1bd3e40b78553558935380a22ccc55faa3bb785
TatyGotica <tatiany.garcia@unesp.br>
2025-03-31 16:22:52 +0000

    Função agregadora de valores númericos: SUM, soma

--- exercicios/2-construindo-consultas/5-funcoes-agregadoras.sql
+++ exercicios/2-construindo-consultas/5-funcoes-agregadoras.sql
@@ -1,14 +1,8 @@
---Funções para manipulação de dados de tipo textual (string)
---REPLACE: usada para mudar uma string de uma coluna específica. 
--- ex prático de uso: REPLACE (Address, 'Av.' , 'Avenida') as endereco
+--Funções para agregar valores númericos 
+--SUM: usada para somar todas as linhas de uma determinada coluna.
+-- representada na query como: SUM(coluna) as soma_coluna
 
-SELECT DISTINCT
-CustomerID as id,
-LOWER (FirstName) as nome,
-UPPER (LastName) as sobrenome,
-FirstName || ' ' || LastName as 
-nome_completo,
-REPLACE (Address, 'Av.', 'Avenida') as endereco
-FROM customers
-WHERE
-Country LIKE 'Bra%';
+SELECT
+SUM(Total) as soma_compras
+FROM
+invoices;
